dfd77f64d66b0a7a281ea4e09c8a0b0d1f20cb73
Tanya Jain <tanyajain0018@gmail.com>
2021-12-03 05:52:57 -0500

    Expanded level and added cars

--- New Unity Project/Assets/Code/MoveRight.cs
+++ New Unity Project/Assets/Code/MoveRight.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class MoveRight : MonoBehaviour
 {
     Rigidbody _rigidbody;
     public float m_Speed = 5f;
 
     void Start(){
         _rigidbody= GetComponent<Rigidbody>();
-        // _rigidbody.AddForce(new Vector3(_rigidbody.velocity.x , _rigidbody.velocity.y, -100.0f));
     }
 
     void Update () {
-        transform.Translate(Vector3.back * m_Speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
         Destroy(gameObject, 10); 
-
     }
 }
