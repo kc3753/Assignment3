3bb5b929f44c39cdacd488170c470b9fea72c3dc
Hiroki39 <zhenghongyi2000@hotmail.com>
2021-12-16 19:43:13 -0500

    level 2 prototype and spawner bug fix

--- New Unity Project/Assets/Code/Move.cs
+++ New Unity Project/Assets/Code/Move.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     Rigidbody _rigidbody;
-    //public float m_Speed = 5f;
+    public float m_Speed = 5f;
     // float m_Speed = Random.Range(2.0f, 6.0f);
-    void Start(){
-        _rigidbody= GetComponent<Rigidbody>();
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
         // _rigidbody.AddForce(new Vector3(_rigidbody.velocity.x , _rigidbody.velocity.y, -100.0f));
+        Destroy(gameObject, 10);
     }
 
-    void Update () {
-        transform.Translate(Vector3.back * Random.Range(2.0f, 5.0f) * Time.deltaTime);
-        Destroy(gameObject, 15); 
+    void Update()
+    {
+        transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
     }
 }
--- New Unity Project/Assets/Code/Spawner.cs
+++ New Unity Project/Assets/Code/Spawner.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
     public float spawntimer;
     public Transform spawnPos;
-    float currTime = 0;
-    void spawn(){
 
-        GameObject newbullet = Instantiate(bulletPrefab, spawnPos.position, Quaternion.identity);
+    public enum Dir { left, right };
+    float currTime = 0;
+
+    public Dir direction;
+    void spawn()
+    {
+        prefabList.Add(Prefab1);
+        prefabList.Add(Prefab2);
+        prefabList.Add(Prefab3);
+
+        int prefabIndex = UnityEngine.Random.Range(0, 3);
+        GameObject newVehicle = Instantiate(prefabList[prefabIndex], spawnPos.position, Quaternion.identity);
+        if (direction == Dir.left)
+        {
+            print("Debug");
+            newVehicle.transform.Rotate(0, 180, 0);
+        }
         //newbullet.transform.Rotate (new Vector3(0 , 45, 0));
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
         currTime += Time.deltaTime;
-        if(currTime >= spawntimer){
+        if (currTime >= spawntimer)
+        {
             spawn();
             currTime = 0;
         }
